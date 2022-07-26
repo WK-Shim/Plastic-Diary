d0d48e89080e3df623128c937a0adfd83f47da9f
WK-Shim <aaashim090125@gmail.com>
2022-07-26 21:36:59 +0900

    220726-2

---
+++ source/application/Plastic-Diary/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
+import { styles } from "./src/style.js";
+import MainView from "./src/mainView";
+
+const App = () => {
+  return (
+    <MainView />
+  );
+}
+
+export default App;
---
+++ source/application/Plastic-Diary/src/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    topBar: {
+        flex: 1
+    },
+    scrollView: {
+        flex: 4
+    }
+});
+
+export { styles };
